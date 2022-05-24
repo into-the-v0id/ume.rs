2eaa527d572ed023e83f840ec0184113b85ed151
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-05-24 16:30:50 +0200

    refactored encoder

--- src/ume8/encode.rs
+++ src/ume8/encode.rs
@@ -1,4 +1,3 @@
-use std::array::IntoIter;
 use crate::ume8::{MASK_SEQ, MASK_SEQ_CONT_DATA, MASK_SEQ_END, MASK_SEQ_START, MASK_SEQ_START_DATA};
 
 #[derive(Clone)]
@@ -7,7 +6,7 @@ pub struct EncodeUnchecked<I>
     where I: Iterator<Item=u32>
 {
     pub iter: I,
-    current_sequence: IntoIter<u8, 4>,
+    current_sequence: Option<EncodeSequenceUnchecked>,
 }
 
 impl <I> EncodeUnchecked<I>
@@ -17,10 +16,7 @@ impl <I> EncodeUnchecked<I>
     pub fn new(iter: I) -> Self {
         Self {
             iter,
-            current_sequence: unsafe { IntoIter::new_unchecked(
-                std::mem::transmute([0u8, 0u8, 0u8, 0u8]),
-                0..0
-            ) },
+            current_sequence: None,
         }
     }
 }
@@ -32,68 +28,81 @@ impl <I> Iterator for EncodeUnchecked<I>
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        self.current_sequence.next()
+        if self.current_sequence.is_none() {
+            self.current_sequence = Some(EncodeSequenceUnchecked::new(self.iter.next()?));
+        }
+
+        self.current_sequence
+            .as_mut()
+            .unwrap()
+            .next()
             .or_else(|| {
-                self.current_sequence = encode_sequence_unchecked(self.iter.next()?);
+                self.current_sequence = None;
                 self.next()
             })
     }
 }
 
+#[derive(Clone)]
+pub struct EncodeSequenceUnchecked {
+    data: u32,
+    current: u8,
+}
 
-#[inline]
-fn encode_sequence_unchecked(data: u32) -> IntoIter<u8, 4> {
-    // 1 byte
-    if data & 0b1111_1111_1111_1111_1111_1111_1000_0000 == 0 {
-        return unsafe { IntoIter::new_unchecked(
-            std::mem::transmute([
-                data as u8,
-                0u8,
-                0u8,
-                0u8,
-            ]),
-            0..1
-        ) };
+impl EncodeSequenceUnchecked {
+    pub fn new(data: u32) -> Self {
+        Self {
+            data,
+            current: 0,
+        }
     }
+}
 
-    // 2 bytes
-    if data & 0b1111_1111_1111_1111_1111_1000_0000_0000 == 0 {
-        return unsafe { IntoIter::new_unchecked(
-            std::mem::transmute([
-                (((data >> 5) as u8) & MASK_SEQ_START_DATA) | MASK_SEQ | MASK_SEQ_START,
-                ((data as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ | MASK_SEQ_END,
-                0u8,
-                0u8,
-            ]),
-            0..2
-        ) };
+impl Iterator for EncodeSequenceUnchecked {
+    type Item = u8;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.current += 1;
+
+        // 1 byte
+        if self.data & 0b1111_1111_1111_1111_1111_1111_1000_0000 == 0 {
+            return match self.current - 1 {
+                0 => Some(self.data as u8),
+                _ => None,
+            };
+        }
+
+        // 2 bytes
+        if self.data & 0b1111_1111_1111_1111_1111_1000_0000_0000 == 0 {
+            return match self.current - 1 {
+                0 => Some((((self.data >> 5) as u8) & MASK_SEQ_START_DATA) | MASK_SEQ | MASK_SEQ_START),
+                1 => Some(((self.data as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ | MASK_SEQ_END),
+                _ => None,
+            };
+        }
+
+        // 3 bytes
+        if self.data & 0b1111_1111_1111_1111_0000_0000_0000_0000 == 0 {
+            return match self.current - 1 {
+                0 => Some((((self.data >> (5+5)) as u8) & MASK_SEQ_START_DATA) | MASK_SEQ | MASK_SEQ_START),
+                1 => Some((((self.data >> 5) as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ),
+                2 => Some(((self.data as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ | MASK_SEQ_END),
+                _ => None,
+            };
+        }
+
+        // 4 bytes
+        if self.data & 0b1111_1111_1110_0000_0000_0000_0000_0000 == 0 {
+            return match self.current - 1 {
+                0 => Some((((self.data >> (5+5+5)) as u8) & MASK_SEQ_START_DATA) | MASK_SEQ | MASK_SEQ_START),
+                1 => Some((((self.data >> (5+5)) as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ),
+                2 => Some((((self.data >> 5) as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ),
+                3 => Some(((self.data as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ | MASK_SEQ_END),
+                _ => None,
+            };
+        }
+
+        panic!("trying to encode more than 21 bits of data");
     }
-
-    // 3 bytes
-    if data & 0b1111_1111_1111_1111_0000_0000_0000_0000 == 0 {
-        return unsafe { IntoIter::new_unchecked(
-            std::mem::transmute([
-                (((data >> (5+5)) as u8) & MASK_SEQ_START_DATA) | MASK_SEQ | MASK_SEQ_START,
-                (((data >> 5) as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ,
-                ((data as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ | MASK_SEQ_END,
-                0u8,
-            ]),
-            0..3
-        ) };
-    }
-
-    // 4 bytes
-    if data & 0b1111_1111_1110_0000_0000_0000_0000_0000 == 0 {
-        return unsafe { IntoIter::new_unchecked(
-            std::mem::transmute([
-                (((data >> (5+5+5)) as u8) & MASK_SEQ_START_DATA) | MASK_SEQ | MASK_SEQ_START,
-                (((data >> (5+5)) as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ,
-                (((data >> 5) as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ,
-                ((data as u8) & MASK_SEQ_CONT_DATA) | MASK_SEQ | MASK_SEQ_END,
-            ]),
-            0..4
-        ) };
-    }
-
-    panic!("trying to encode more than 21 bits of data");
 }
