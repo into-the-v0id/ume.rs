a430970aa17b6a2c5de6ef1db516d15bb866f386
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-05-27 18:44:58 +0200

    fixed bug when counting sequences

--- src/ume8/util.rs
+++ src/ume8/util.rs
@@ -1,8 +1,8 @@
-use crate::ume8::MASK_SEQ_START;
+use crate::ume8::{MASK_SEQ, MASK_SEQ_START};
 
 pub fn count_sequences_unchecked<I: Iterator<Item=u8>>(iter: I) -> usize {
     iter
-        .filter(|byte| byte & MASK_SEQ_START != 0)
+        .filter(|byte| byte & MASK_SEQ == 0 || byte & MASK_SEQ_START != 0)
         .count()
 }
 
