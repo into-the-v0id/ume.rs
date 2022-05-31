9820ab2151f194fbea8140f1b42b17a6072f1501
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-05-31 13:42:26 +0200

    moved some methods from string to str

--- src/ume8/str.rs
+++ src/ume8/str.rs
@@ -1,6 +1,9 @@
 use std::borrow::Borrow;
 use std::fmt::{Debug, Display, Formatter};
+use crate::DecodeUnchecked;
+use crate::ume8::decode::ToCharUnchecked;
 use crate::ume8::string::String as Ume8String;
+use crate::ume8::util::is_singleton;
 
 #[repr(transparent)]
 #[derive(PartialOrd, PartialEq, Ord, Eq, Hash)]
@@ -22,15 +25,83 @@ impl Str {
     pub(crate) unsafe fn from_inner_mut(inner: &mut [u8]) -> &mut Self {
         unsafe { &mut *(inner as *mut [u8] as *mut Str) }
     }
-}
 
-impl Str {
+    pub fn len(&self) -> usize {
+        self.bytes.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.bytes.is_empty()
+    }
+
     pub fn as_bytes(&self) -> &[u8] {
         &self.bytes
     }
 
     pub fn as_bytes_mut(&mut self) -> &mut [u8] {
         &mut self.bytes
+    }
+
+    pub fn chars(&self) -> ToCharUnchecked<DecodeUnchecked<std::vec::IntoIter<u8>>> {
+        unsafe {
+            ToCharUnchecked::new(
+                DecodeUnchecked::new(
+                    self.bytes.to_owned().into_iter()
+                )
+            )
+        }
+    }
+
+    // TODO
+    // pub fn lines(&self) -> Iterator {
+    //
+    // }
+
+    pub fn contains(&self, other: &Self) -> bool {
+        if other.bytes.len() == 0 {
+            return true;
+        }
+
+        self.bytes.windows(other.bytes.len())
+            .any(|chunk| &other.bytes == chunk)
+    }
+
+    pub fn starts_with(&self, other: &Self) -> bool {
+        self.bytes.starts_with(&other.bytes)
+    }
+
+    pub fn ends_with(&self, other: &Self) -> bool {
+        self.bytes.ends_with(&other.bytes)
+    }
+
+    // TODO
+    // pub fn split(&self) -> Iterator {
+    //
+    // }
+
+    // TODO
+    // pub fn trim(&self) -> Self {
+    //
+    // }
+
+    // TODO
+    // pub fn trim_start(&self) -> Self {
+    //
+    // }
+
+    // TODO
+    // pub fn trim_left(&self) -> Self {
+    //
+    // }
+
+    // TODO
+    // pub fn parse(&self) -> T {
+    //
+    // }
+
+    pub fn is_ascii(&self) -> bool {
+        self.bytes.iter()
+            .all(|byte| is_singleton(byte))
     }
 }
 
--- src/ume8/string.rs
+++ src/ume8/string.rs
@@ -3,10 +3,8 @@ use std::ops::{Deref, DerefMut, Index, IndexMut, RangeFull};
 use std::str::FromStr;
 use std::string::String as StdString;
 use crate::EncodeUnchecked;
-use crate::ume8::decode::{DecodeUnchecked, ToCharUnchecked};
 use crate::ume8::encode::EncodeSequenceUnchecked;
 use crate::ume8::str::Str;
-use crate::ume8::util::is_singleton;
 
 #[repr(transparent)]
 #[derive(PartialOrd, PartialEq, Ord, Eq, Hash)]
@@ -80,86 +78,6 @@ impl String {
 
     pub fn clear(&mut self) {
         self.bytes.clear();
-    }
-}
-
-impl String {
-    pub fn len(&self) -> usize {
-        self.bytes.len()
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.bytes.is_empty()
-    }
-
-    pub fn as_bytes(&self) -> &[u8] {
-        &self.bytes
-    }
-
-    pub fn as_bytes_mut(&mut self) -> &mut [u8] {
-        &mut self.bytes
-    }
-
-    pub fn chars(&self) -> ToCharUnchecked<DecodeUnchecked<std::vec::IntoIter<u8>>> {
-        unsafe {
-            ToCharUnchecked::new(
-                DecodeUnchecked::new(
-                    self.bytes.clone().into_iter()
-                )
-            )
-        }
-    }
-
-    // TODO
-    // pub fn lines(&self) -> Iterator {
-    //
-    // }
-
-    pub fn contains(&self, other: &Self) -> bool {
-        if other.bytes.len() == 0 {
-            return true;
-        }
-
-        self.bytes.windows(other.bytes.len())
-            .any(|chunk| &other.bytes == chunk)
-    }
-
-    pub fn starts_with(&self, other: &Self) -> bool {
-        self.bytes.starts_with(&other.bytes)
-    }
-
-    pub fn ends_with(&self, other: &Self) -> bool {
-        self.bytes.ends_with(&other.bytes)
-    }
-
-    // TODO
-    // pub fn split(&self) -> Iterator {
-    //
-    // }
-
-    // TODO
-    // pub fn trim(&self) -> Self {
-    //
-    // }
-
-    // TODO
-    // pub fn trim_start(&self) -> Self {
-    //
-    // }
-
-    // TODO
-    // pub fn trim_left(&self) -> Self {
-    //
-    // }
-
-    // TODO
-    // pub fn parse(&self) -> T {
-    //
-    // }
-
-    pub fn is_ascii(&self) -> bool {
-        self.bytes.iter()
-            .all(|byte| is_singleton(byte))
     }
 }
 
