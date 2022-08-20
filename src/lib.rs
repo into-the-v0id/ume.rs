026a9b32221775bf7c23d0990a7e27bc728ebe6f
Void <57257404+into-the-v0id@users.noreply.github.com>
2022-08-20 12:00:06 +0200

    Added license

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
+// Copyright Oliver Amann
+//
+// Licensed under the MIT License or the Apache License Version 2.0,
+// at your option. You may not use this file except according to
+// those terms.
+
 pub mod ume8;
